9d6504684ae95e62cecc61eea8946ba75255010c
peeweeid <65776660+peeweeid@users.noreply.github.com>
2023-04-27 14:18:21 +0700

    Update [id].tsx

--- pages/Services/[id].tsx
+++ pages/Services/[id].tsx
@@ -47,16 +47,19 @@ const Pokeid= ({dataService}:any) => {
 
                                 <input type="text" id="nama"  value={todo.nama}
                                     onChange={e => setTodo({...todo, nama: e.target.value})} 
+                                    required={true} minLength={3} maxLength={10} 
                                     className="mt-4 px-3 py-2 bg-secondary border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full  sm:text-sm focus:ring-1" placeholder="nama pemesan" />
                                 
 
                                 <input type="text" id="porsi"  value={todo.porsi}
                                     onChange={e => setTodo({...todo, porsi: e.target.value})} 
+                                    required={true} minLength={1} maxLength={4} 
                                     className="mt-4 px-3 py-2 bg-secondary border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full  sm:text-sm focus:ring-1" placeholder="banyak porsi" />
                                 
                                 
                                 <input type="date" id="waktu"  value={todo.waktu}
                                     onChange={e => setTodo({...todo, waktu: e.target.value})} 
+                                      required={true} minLength={1} maxLength={400} 
                                     className="mt-4 px-3 py-2 bg-secondary border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full  sm:text-sm focus:ring-1" placeholder="untuh tanggal" />
 
                                 <select id="underline_select" value={todo.paket} onChange={e => setTodo({...todo, paket: e.target.value})}  className="block py-2.5 px-0 w-full text-sm mt-5 text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
@@ -72,7 +75,7 @@ const Pokeid= ({dataService}:any) => {
                                     }
                                 </select>
                             <div className='flex items-center justify-center'>
-                                <button onClick={onSubmit} className='text-xl font-bold rounded-lg  bg-primary hover:bg-third px-8 py-2 mt-5 w-full text-white'> Order </button>
+                                <button type='submit' className='text-xl font-bold rounded-lg  bg-primary hover:bg-third px-8 py-2 mt-5 w-full text-white'> Order </button>
                             </div>
                         </div>
                         
