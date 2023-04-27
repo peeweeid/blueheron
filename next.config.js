f445a66895571488780a901240d23e5b30d5045c
peeweeid <p2eweeworld@gmail.com>
2023-04-27 14:05:50 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+const withImages = require("next-images");
+const withTM = require("next-transpile-modules")(["@madzadev/image-slider"]);
+module.exports = nextConfig
+module.exports = withImages(withTM());
